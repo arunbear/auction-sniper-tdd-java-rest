8b77e354d5613d60af71fa13eddb7de060ba345f
Arun Prasaad <arunbear@cpan.org>
2024-08-22 17:25:18 +0100

    Add initial project files

---
+++ src/main/java/org/example/auctionsniper/AuctionSniperApplication.java
@@ -0,0 +1,13 @@
+package org.example.auctionsniper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionSniperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionSniperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/auctionsniper/AuctionSniperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.auctionsniper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionSniperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
