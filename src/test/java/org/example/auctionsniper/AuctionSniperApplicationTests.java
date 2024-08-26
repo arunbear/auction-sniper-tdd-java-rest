0204d9c1479db13f898d0efe69c8240849eabd90
Arun Prasaad <arunbear@cpan.org>
2024-08-26 11:46:02 +0100

    Ch 11. [Fail] Add test step: start bidding in auction

--- src/test/java/org/example/auctionsniper/AuctionSniperApplicationTests.java
+++ src/test/java/org/example/auctionsniper/AuctionSniperApplicationTests.java
@@ -1,13 +1,34 @@
 package org.example.auctionsniper;
 
+import io.restassured.RestAssured;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AuctionSniperApplicationTests {
+    @LocalServerPort
+    private int localServerPort;
 
-    @Test
-    void contextLoads() {
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = localServerPort;
     }
 
+    @Test
+    void sniper_joins_auction_until_auction_closes() {
+        startBiddingInAuction();
+    }
+
+    private void startBiddingInAuction() {
+        RestAssured
+            .get("/")
+            .then()
+            .statusCode(equalTo(HttpStatus.SC_OK))
+            .body("status", equalTo("JOINING"));
+    }
 }
